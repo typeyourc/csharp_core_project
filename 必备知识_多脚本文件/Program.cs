a5982e5fa144b8c78658b48ae382300db3cc64eb
126074784+typeyourc <126074784+typeyourc@users.noreply.github.com>
2023-03-20 23:35:24 +0800

    添加项目文件。

---
+++ 必备知识_多脚本文件/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace 必备知识_多脚本文件
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
